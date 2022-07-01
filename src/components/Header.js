dcbd78a7037a7bdc0b8509a9320b96085185900a
guisantosalves <workguisantos@gmail.com>
2022-06-30 22:46:10 -0400

    doing header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,34 @@
 import React from "react";
 import "./css/Header.css"
+import PaidIcon from '@mui/icons-material/Paid';
+import { Avatar } from "@mui/material";
 
 function Header(){
     return(
         <div className="header">
-            <h1>my header</h1>
+            <div className="header__left">
+                <img src="https://cdn-icons-png.flaticon.com/512/3135/3135706.png"/>
+                
+                <div className="header__leftInformation">
+                    <h3>My Own Money</h3>
+                    <p>for you control your money</p>
+                </div>
+            </div>
+
+            <div className="header__center">
+                <div>ganhos</div>
+                <div>alimentação</div>
+                <div>lazer</div>
+                <div>investimento</div>
+                <div>transporte</div>
+                <div>outros</div>
+            </div>
+
+            <div className="header_right">
+                <div>Guilherme santos</div>
+                <Avatar src="https://i.pinimg.com/564x/bd/ac/a5/bdaca5afbf28608dd2305bece054c424.jpg"/>
+            </div>
+            
         </div>
     )
 }
