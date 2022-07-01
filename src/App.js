f0e7e9316a41072dc9f541b1782653a9c5cd82cb
guisantosalves <workguisantos@gmail.com>
2022-06-30 21:38:54 -0400

    initializing my header and create the project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+      
+      {/* header */}
+      <Header/>
+
+      {/* body page with router -> home e as classificações */}
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./css/Header.css"
+
+function Header(){
+    return(
+        <div className="header">
+            <h1>my header</h1>
+        </div>
+    )
+}
+
+export default Header;
