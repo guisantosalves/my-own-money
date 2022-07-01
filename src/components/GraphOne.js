366bcd7db0e363d6246b88263597cdab2026524a
guisantosalves <workguisantos@gmail.com>
2022-06-30 23:08:59 -0400

    graphs inserted with success

---
+++ src/components/GraphOne.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import "./css/GraphOne.css"
+import Chart from "react-apexcharts";
+
+function GraphOne(){
+
+    const [setting, setSetting] = useState([{
+        options: {
+            chart: {
+              id: "basic-bar"
+            },
+            xaxis: {
+              categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
+            }
+          },
+        series: [
+            {
+              name: "series-1",
+              data: [30, 40, 45, 50, 49, 60, 70, 91]
+            }
+          ]
+        
+    }
+    ])
+
+    return(
+        <div className="graphOne">
+
+            {setting.map((item, index)=>(
+                <Chart
+                options={item.options}
+                series={item.series}
+                type="line"
+                width="400"
+              />
+            ))}
+
+        </div>
+    )
+}
+
+export default GraphOne;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,39 @@
 import React from "react";
 import "./css/Home.css";
 import Row from "../components/RowExpend";
+import GraphOne from "../components/GraphOne";
+import GraphTwo from "../components/GraphTwo";
 
-function Home(){
-    return(
-        <div className="home">
+function Home() {
+  return (
+    <div className="home">
+      <div className="home__left">
 
-            <div className="home__left">
-                <h1>grafico</h1>
-            </div>
+        <div className="home__graphs">
+          <div className="home__leftGraphOne">
+            <GraphOne />
+          </div>
 
-            <div className="home__right">
-                <div className="home__rightListOfThings">
-                    {Array(10).fill().map(()=>(
-                        <Row expend={1234} expendType={`comida`} />
-                    ))}
-                </div>
-            </div>
-
+          <div className="home__leftGraphTwo">
+            <GraphTwo />
+          </div>
         </div>
-    )
+
+      </div>
+
+      <div className="home__right">
+
+        <div className="home__rightListOfThings">
+          {Array(10)
+            .fill()
+            .map(() => (
+              <Row expend={1234} expendType={`comida`} />
+            ))}
+        </div>
+
+      </div>
+    </div>
+  );
 }
 
 export default Home;
