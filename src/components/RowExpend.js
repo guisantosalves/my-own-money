991159d81ac6e7eb34dc263247a4ed0b166b3df0
guisantosalves <workguisantos@gmail.com>
2022-07-02 21:39:01 -0400

    conditional redering of icons in row is done

--- src/components/RowExpend.js
+++ src/components/RowExpend.js
@@ -1,23 +1,46 @@
 import React, { useState } from "react";
-import "./css/RowExpend.css"
+import "./css/RowExpend.css";
 import FastfoodIcon from "@mui/icons-material/Fastfood";
+import ChairIcon from "@mui/icons-material/Chair";
+import RocketLaunchIcon from "@mui/icons-material/RocketLaunch";
+import DirectionsBusIcon from "@mui/icons-material/DirectionsBus";
+import AutoAwesomeMotionIcon from "@mui/icons-material/AutoAwesomeMotion";
 
-function Row({expend, expendType}){
-    
-    return(
-        <div className="row">
-            
-            <div className="row__left">
-                <h2>Value: {expend}</h2>
-                <p>Type of expense: {expendType}</p>
-            </div>
+function Row({ expend, expendType, timestamp }) {
+  function IconType() {
+    switch (expendType) {
+      case "food":
+        return <FastfoodIcon fontSize="large" />;
+        break;
 
-            <div className="row__right">
-                <FastfoodIcon fontSize="large"/>
-            </div>
+      case "pleasure":
+        return <ChairIcon fontSize="large" />;
+        break;
 
-        </div>
-    )
+      case "investment":
+        return <RocketLaunchIcon fontSize="large" />;
+        break;
+
+      case "transport":
+        return <DirectionsBusIcon fontSize="large" />;
+        break;
+
+      case "other":
+        return <AutoAwesomeMotionIcon fontSize="large" />;
+        break;
+    }
+  }
+
+  return (
+    <div className="row">
+      <div className="row__left">
+        <h2>Value: {expend}</h2>
+        <p>Type of expense: {expendType}</p>
+      </div>
+
+      <div className="row__right">{IconType()}</div>
+    </div>
+  );
 }
 
 export default Row;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import "./css/Home.css";
 import Row from "../components/RowExpend";
 import GraphOne from "../components/GraphOne";
 import GraphTwo from "../components/GraphTwo";
 
 function Home() {
+  const [data, setData] = useState([
+    {
+      expend: 2548,
+      expendType: "food",
+      timestamp: "15:30",
+    },
+    {
+      expend: 4687,
+      expendType: "transport",
+      timestamp: "15:30",
+    },
+    {
+      expend: 8596,
+      expendType: "investment",
+      timestamp: "15:30",
+    },
+    {
+      expend: 8596,
+      expendType: "investment",
+      timestamp: "15:30",
+    },
+    {
+      expend: 8596,
+      expendType: "investment",
+      timestamp: "15:30",
+    },
+    {
+      expend: 8596,
+      expendType: "investment",
+      timestamp: "15:30",
+    },
+    {
+      expend: 4687,
+      expendType: "transport",
+      timestamp: "15:30",
+    },
+    {
+      expend: 4687,
+      expendType: "transport",
+      timestamp: "15:30",
+    },
+  ]);
+
   return (
     <div className="home">
       <div className="home__left">
-
         <div className="home__graphs">
-
           <div className="home__leftGraphOne">
             <GraphOne />
           </div>
@@ -18,21 +59,19 @@ function Home() {
           <div className="home__leftGraphTwo">
             <GraphTwo />
           </div>
-          
         </div>
-
       </div>
 
       <div className="home__right">
-
         <div className="home__rightListOfThings">
-          {Array(10)
-            .fill()
-            .map(() => (
-              <Row expend={1234} expendType={`comida`} />
-            ))}
+          {data.map((item, index) => (
+            <Row
+              expend={item.expend}
+              expendType={item.expendType}
+              timestamp={item.timestamp}
+            />
+          ))}
         </div>
-
       </div>
     </div>
   );
