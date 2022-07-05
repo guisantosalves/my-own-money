1c2e78ec8fa993d0ae5d6606c6b826bb92a9bd5e
guisantosalves <workguisantos@gmail.com>
2022-07-04 20:15:25 -0400

    fixing the graph

--- src/components/GraphOne.js
+++ src/components/GraphOne.js
@@ -1,42 +1,41 @@
-import React, { useState } from "react";
-import "./css/GraphOne.css"
+import React, { useEffect, useState } from "react";
+import "./css/GraphOne.css";
 import Chart from "react-apexcharts";
 
-function GraphOne(){
+function GraphOne() {
 
-    const [setting, setSetting] = useState([{
-        options: {
-            chart: {
-              id: "basic-bar"
-            },
-            xaxis: {
-              categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
-            }
-          },
-        series: [
-            {
-              name: "series-1",
-              data: [30, 40, 45, 50, 49, 60, 70, 91]
-            }
-          ]
-        
-    }
-    ])
+  const [setting, setSetting] = useState([
+    {
+      options: {
+        chart: {
+          id: "basic-bar",
+        },
+        xaxis: {
+          categories: ["food", "pleasure", "investment", "transport", "other"],
+        },
+      },
+      series: [
+        {
+          //aqui tem que ser os somatórios das categorias
+          name: "Value",
+          data: [15.5, 16.6, 51, 16.6, 16.7],
+        },
+      ],
+    },
+  ]);
 
-    return(
-        <div className="graphOne">
-
-            {setting.map((item, index)=>(
-                <Chart
-                options={item.options}
-                series={item.series}
-                type="line"
-                width="400"
-              />
-            ))}
-
-        </div>
-    )
+  return (
+    <div className="graphOne">
+      {setting.map((item, index) => (
+        <Chart
+          options={item.options}
+          series={item.series}
+          type="line"
+          width="400"
+        />
+      ))}
+    </div>
+  );
 }
 
 export default GraphOne;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -22,6 +22,7 @@ function Home() {
           data: item.data(),
         }
       )))
+
     })
 
   }, [data])
